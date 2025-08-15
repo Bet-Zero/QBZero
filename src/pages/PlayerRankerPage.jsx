531eb37efacbf266c03dd53f39d0c0096c2a7cc3
Bet-Zero <driertoaster1@gmail.com>
2025-08-14 19:32:22 -0500

    feat: add quarterback list for ranker

--- src/pages/PlayerRankerPage.jsx
+++ src/pages/PlayerRankerPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import RankingBuilder from '@/features/ranker/RankingBuilder';
+import RankingSession from '@/features/ranker/RankingSession';
+import { quarterbacks } from '@/features/ranker/quarterbacks';
 import '@/features/ranker/ranker.css';
 
 const PlayerRankerPage = () => (
   <div className="bg-neutral-900 min-h-screen text-white">
-    <RankingBuilder />
+    <RankingSession playerPool={quarterbacks} />
   </div>
 );
 
